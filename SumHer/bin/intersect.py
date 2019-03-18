b9a0f62fb8eefefb34e7bb88707b7e0910f1ee22
Daniel Taliun <dtaliun@umich.edu>
2019-03-18 09:50:27 -0400

    speedup intersect

--- SumHer/bin/intersect.py
+++ SumHer/bin/intersect.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import argparse
-import os
+from collections import Counter
 
 argparser = argparse.ArgumentParser(description = 'Intersects files preserving the order. Note: suitable only for very short lines (e.g. rsId).')
 argparser.add_argument('-i', '--input', metavar = 'file', dest = 'in_files', nargs = '+', required = True, help = 'Input files.')
@@ -10,23 +10,13 @@ argparser.add_argument('-o', '--out', metavar = 'file', dest = 'out_file', requi
 
 if __name__ == '__main__':
     args = argparser.parse_args()
-    sizes = []
+    n_files = len(args.in_files)
+    names = Counter()
     for in_file in args.in_files:
-        sizes.append((os.stat(in_file).st_size, in_file))
-    sizes.sort(key = lambda x: x[0])
-    names = dict()
-    with open(sizes[0][1], 'rt') as ifile:
-        for line in ifile:
-            line = line.rstrip()
-            if line: names[line] = 1
-    for size, in_file in sizes[1:]:
-        with open(in_file, 'rt') as ifile:
+        with open(in_file, 'rb') as ifile:
+            names.update(ifile)
+    with open(args.out_file, 'wb') as ofile:
+        with open(args.in_files[0], 'rb') as ifile:
             for line in ifile:
-                line = line.rstrip()
-                if line in names: names[line] += 1
-    with open(args.out_file, 'wt') as ofile:
-        with open(sizes[0][1], 'rt') as ifile:
-            for line in ifile:
-                line = line.rstrip()
-                if names[line] == len(sizes):
-                    ofile.write('{}\n'.format(line))
+                if names[line] == n_files:
+                    ofile.write(line)
